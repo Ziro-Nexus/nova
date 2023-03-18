ae3db9520879aafa7786764a8729c17872ef5aac
Zirox <ziroxdeveloper@outlook.com>
2023-03-18 09:10:44 -0500

    add

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,15 @@ mod tokenizer;
 use tokenizer::token::Token;
 
 fn main() {
-    let token = Token::new("main.ZPP");
+
+    let st = "
+        alloc type:s Greeting = 'hola' + 'mundo';
+        if 5 > 2 :
+            @print 2 + 3 * 20 - 10 / 2
+        ;
+    ";
+
+    let token = Token::new(st, false);
     println!("{:#?}", token);
     println!("{:#?}", token.generate_subtokens());
 }
--- src/tokenizer/token.rs
+++ src/tokenizer/token.rs
@@ -8,11 +8,16 @@ enum MatchHandler {
     AllocatorAssign,
     Procedure,
     ProcedureTrigger,
-    ProcedureSeparator,
+    Then,
     TypeNum,
     TypeNumDecimal,
     TypeBool,
     TypeString,
+    LiteralNum,
+    LiteralDecimal,
+    LiteralString,
+    LiteralBoolTrue,
+    LiteralBoolFalse,
     ExprSum,
     ExprSubs,
     ExprMultiplication,
@@ -26,18 +31,22 @@ enum MatchHandler {
     Unknown
 }
 
+// It is used to define the behavior of a subtoken
 impl MatchHandler {
     pub fn new(item: &String) -> Self {
-        match item.as_str() {
+
+        let mut mtchandler = MatchHandler::Unknown;
+
+        mtchandler = match item.as_str() {
+            // Matching handler for general keys
             "alloc" => MatchHandler::AllocatorVar,
             "=" => MatchHandler::AllocatorAssign,
             "proc"=> MatchHandler::Procedure,
-            "@" => MatchHandler::ProcedureTrigger,
             "type:n" => MatchHandler::TypeNum,
             "type:f" => MatchHandler::TypeNumDecimal,
             "type:b" => MatchHandler::TypeBool,
             "type:s" => MatchHandler::TypeString,
-            ":" => MatchHandler::ProcedureSeparator,
+            ":" => MatchHandler::Then,
             "+" => MatchHandler::ExprSum,
             "-" => MatchHandler::ExprSubs,
             "*" => MatchHandler::ExprMultiplication,
@@ -47,10 +56,55 @@ impl MatchHandler {
             "<" => MatchHandler::Less,
             "eq" => MatchHandler::Equal,
             "not" => MatchHandler::ISNT,
+            // the las match of the general keyboards is the procedure trigger (function call)
+            &_ => match item.starts_with("@") {
+                true => MatchHandler::ProcedureTrigger,
+                false => MatchHandler::Unknown
+            }
+        };
 
+        // check for type "type:n"
+        mtchandler = match item.parse::<u32>().is_ok() {
+            true => MatchHandler::LiteralNum,
+            false => mtchandler
+        };
 
-            &_ => MatchHandler::Unknown,
+        // if the item is already a number, no needs more matchings
+        if let MatchHandler::LiteralNum = mtchandler {
+            return mtchandler;
         }
+
+        // check for type "type:f"
+        mtchandler = match item.parse::<f32>().is_ok() {
+            true => MatchHandler::LiteralDecimal,
+            false => mtchandler
+        };
+
+        // if the item is already a decimal, no needs more matchings
+        if let MatchHandler::LiteralDecimal = mtchandler {
+            return mtchandler;
+        }
+
+        // check for type type:s
+        // literal string should start with ' and ends with '
+        mtchandler = match item.starts_with("'") {
+            true => match item.ends_with("'") {
+                true => MatchHandler::LiteralString,
+                false => mtchandler
+            },
+            false => mtchandler
+        };
+
+
+        // check for type type:b
+        mtchandler = match item.as_str() {
+            "true" => MatchHandler::LiteralBoolTrue,
+            "false" => MatchHandler::LiteralBoolFalse,
+
+            _ => mtchandler
+        };
+
+        mtchandler
     } 
 }
 
@@ -89,10 +143,15 @@ pub struct Token {
 
 // Create a new Token
 impl Token {
-    pub fn new(codebase_path: &str) -> Self {
-        let codebase = fs::read_to_string(codebase_path).unwrap_or_else(|err| {
-            panic!("{}", err.to_string())
-        });
+    pub fn new(codebase_path: &str, file_path: bool) -> Self {
+
+        let mut codebase: String = String::from(codebase_path);
+
+        if file_path {
+            codebase = fs::read_to_string(codebase_path).unwrap_or_else(|err| {
+                panic!("{}", err.to_string())
+            });
+        }
 
         println!("{:?}", codebase.as_bytes());
 
@@ -119,14 +178,22 @@ impl Token {
         let mut sub_tokens: Vec<SubToken> = Vec::new();
 
         for (item_id, item) in self.items.iter().enumerate() {
-            // sb = subtokens
-            let sb: Vec<String> = item.split(" ")
-                .map(|x| String::from(x))
-                .filter(|x| x.len() > 0)
-                .collect();
-            for (id, sub_item) in sb.iter().enumerate() {
-                sub_tokens.push(SubToken::new(id, sub_item.clone(), item_id));
+            // detect if the line contains a literal string or some
+            if !item.starts_with("'") && !item.ends_with("'") {
+                let sb: Vec<String> = item.split(" ")
+                    .map(|x| String::from(x))
+                    .filter(|x| x.len() > 0)
+                    .collect();
+                for (id, sub_item) in sb.iter().enumerate() {
+                    sub_tokens.push(SubToken::new(id, sub_item.clone(), item_id));
+                }
+            } else {
+                // literal string detection in the current line!
+                for i in item.split_ascii_whitespace().enumerate() {
+                    sub_tokens.push(SubToken::new(i.0, i.1.to_string(), item_id));
+                }
             }
+            
 
         }
 
